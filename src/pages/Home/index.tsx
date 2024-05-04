a141dfe5126602e86c0892ce2abc0a68ef637c09
degui1 <gui.denez56@gmail.com>
2024-05-04 17:53:13 -0300

    Finalizando o feed da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,72 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 import FeedImg from '../../assets/FeedImg.svg'
-import { HomeContainer, SectionContainer } from './styles'
+import { Feed, HomeContainer, Info, SectionContainer } from './styles'
+import { useTheme } from 'styled-components'
 
 export function Home() {
+  const theme = useTheme()
+
+  console.log(theme)
+
   return (
     <HomeContainer>
-      <SectionContainer>
-        <article className="HomeArticleSection">
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <h3>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </h3>
-        </article>
+      <Feed>
+        <SectionContainer>
+          <article className="HomeArticleSection">
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <h3>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </h3>
+          </article>
 
-        <ul>
-          <li>Compra simples e segura</li>
-          <li>Entrega rápida e rastreada</li>
-          <li>Embalagem mantém o café intacto</li>
-          <li>O café chega fresquinho até você</li>
-        </ul>
-      </SectionContainer>
-      <img src={FeedImg} alt="" className="feedImage" />
+          <Info>
+            <div>
+              <ShoppingCart
+                size={32}
+                weight="fill"
+                color={theme.background}
+                style={{ backgroundColor: theme['yellow-dark'] }}
+              />
+
+              <span>Compra simple e segura</span>
+            </div>
+
+            <div>
+              <Package
+                size={32}
+                weight="fill"
+                color={theme.background}
+                style={{ backgroundColor: theme['base-text'] }}
+              />
+
+              <span>Embalagem mantém o café intacto</span>
+            </div>
+
+            <div>
+              <Timer
+                size={32}
+                weight="fill"
+                color={theme.background}
+                style={{ backgroundColor: theme.yellow }}
+              />
+
+              <span>Entrega rápida e rastreada</span>
+            </div>
+            <div>
+              <Coffee
+                size={32}
+                weight="fill"
+                color={theme.background}
+                style={{ backgroundColor: theme.purple }}
+              />
+
+              <span>O café chega frequinho até você</span>
+            </div>
+          </Info>
+        </SectionContainer>
+        <img src={FeedImg} alt="" className="feedImage" />
+      </Feed>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,23 +3,53 @@ import { mixins } from '../../styles/mixins'
 
 export const HomeContainer = styled.main`
   display: flex;
+  flex-direction: column;
+
   height: calc(100vh - 6.5rem);
-
-  align-items: center;
-
+  padding-top: 5.875rem;
   max-width: 1160px;
+`
+
+export const Feed = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 3.5rem;
 `
 
 export const SectionContainer = styled.section`
   display: flex;
   flex-direction: column;
+  gap: 3.5rem;
 
-  h1 {
-    ${mixins.fonts.titleXL}
+  article {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    & h1 {
+      ${mixins.fonts.titleXL}
+    }
+
+    & h3 {
+      ${mixins.fonts.textL}
+      color: ${(props) => props.theme.subtitle};
+    }
   }
+`
 
-  h3 {
-    ${mixins.fonts.textL}
-    color: ${(props) => props.theme.subtitle};
+export const Info = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-row-gap: 20px;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    svg {
+      padding: 8px;
+      border-radius: 999px;
+    }
   }
 `
