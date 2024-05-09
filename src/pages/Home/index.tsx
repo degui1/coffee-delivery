ac305888df1e6d7d54336343c8195fb6da814571
degui1 <gui.denez56@gmail.com>
2024-05-09 09:41:42 -0300

    card component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,47 @@
+import { Minus, Plus, ShoppingCart } from 'phosphor-react'
 import { Feed } from './components/Feed'
-import { HomeContainer, Menu } from './styles'
+import { BuySection, Card, Counter, HomeContainer, Menu } from './styles'
+
+import Expresso from '../../assets/coffees/Expresso.svg'
+import { useTheme } from 'styled-components'
 
 export function Home() {
+  const theme = useTheme()
+
   return (
     <HomeContainer>
       <Feed />
       <Menu>
         <h1>Nossos Cafés</h1>
+        <Card>
+          <img src={Expresso} alt="" />
+          <span className="pill">Tradicional</span>
+          <h3>Expresso tradicional</h3>
+          <p>O tradicional café feito com água quente e grãos moídos</p>
+
+          <BuySection>
+            <span>
+              R$ <span className="cardPrice">9,90</span>
+            </span>
+            <Counter>
+              <span>
+                <Minus size={14} color={theme.purple} />
+              </span>
+              <span className="counterNumber">0</span>
+              <span>
+                <Plus size={14} color={theme.purple} />
+              </span>
+            </Counter>
+            <button>
+              <ShoppingCart
+                size={32}
+                weight="fill"
+                color={theme.background}
+                style={{ backgroundColor: theme['purple-dark'] }}
+              />
+            </button>
+          </BuySection>
+        </Card>
       </Menu>
     </HomeContainer>
   )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { mixins } from '../../styles/mixins'
 
 export const HomeContainer = styled.main`
   display: flex;
@@ -11,4 +12,84 @@ export const HomeContainer = styled.main`
 
 export const Menu = styled.div`
   padding: 2rem 0;
+`
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  background-color: ${(props) => props.theme['base-card']};
+  border-radius: 6px 36px;
+  height: 310px;
+  width: 256px;
+
+  padding: 20px 24px;
+
+  justify-content: space-between;
+  align-items: center;
+
+  text-align: center;
+
+  .pill {
+    background-color: ${(props) => props.theme['yellow-light']};
+    ${mixins.fonts.tag}
+    text-transform: uppercase;
+    color: ${(props) => props.theme['yellow-dark']};
+    padding: 4px 2px;
+    border-radius: 100px;
+    text-align: center;
+    width: 80px;
+  }
+
+  h3 {
+    ${mixins.fonts.titleS}
+  }
+
+  p {
+    ${mixins.fonts.textS}
+  }
+
+  img {
+    width: 100px;
+    margin-top: calc(0px - 2rem - 8px);
+  }
+`
+
+export const BuySection = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+
+  justify-content: space-between;
+
+  .cardPrice {
+    ${mixins.fonts.titleM}
+  }
+
+  button {
+    border: none;
+
+    svg {
+      padding: 8px;
+      border-radius: 6px;
+    }
+  }
+`
+
+export const Counter = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  justify-content: space-around;
+  align-items: center;
+
+  background-color: ${(props) => props.theme['base-button']};
+
+  width: 4.5rem;
+  border-radius: 6px;
+
+  .counterNumber {
+    ${mixins.fonts.textM}
+    color: ${(props) => props.theme['base-title']};
+  }
 `
